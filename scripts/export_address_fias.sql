a0503b8cd703db03337c213c67cc5b508f3f86f5
lobo <buzzypom@gmail.com>
2022-07-16 23:42:10 +0800

    Небольшая доработка в экране списка адресов.

--- scripts/export_address_fias.sql
+++ scripts/export_address_fias.sql
@@ -1,14 +1,16 @@
-copy
-(select src_address as address,  house_id as fias, postalcode as postalcode from fias_address_house_link
-join fias_house h on house_id=h.id
-right join fias_address a on address_id=a.id
-group by postalcode, address_id, house_id, src_address
-having count(address_id)=1
-order by src_address)
+copy (
+    select src_address as address,  house_id as fias, postalcode as postalcode, a.cadastral_number as kn from fias_address_house_link
+    join fias_house h on house_id=h.id
+    right join fias_address a on address_id=a.id
+    where a.update_ts>'2022-07-15' and housecounter=1
+    group by postalcode, address_id, house_id, src_address, kn
+    having count(address_id)=1
+    order by src_address
+)
 to '/tmp/address_house_fias.csv' csv header delimiter ';';
 
 copy
-(select src_address as address,  stead_id as fias from fias_address_stead_link
+(select src_address as address,  stead_id as fias, a.cadastral_number as kn from fias_address_stead_link
  right join fias_address a on address_id=a.id
  group by address_id, stead_id, src_address
  having count(address_id)=1
