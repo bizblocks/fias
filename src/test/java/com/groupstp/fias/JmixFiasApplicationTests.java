dfd6644e643eb00b4571a2d67853b0025a207b71
lobo <buzzypom@gmail.com>
2022-11-27 13:49:28 +0800

    migration to jmix

---
+++ src/test/java/com/groupstp/fias/JmixFiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupstp.fias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixFiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
